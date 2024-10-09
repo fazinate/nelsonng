7978d2b0120b02aaea1afbf979a35ffb3196bb06
NelsonNg <sale.qn@gmail.com>
2024-10-09 09:52:45 +0700

    feat: Updated file to first deploy

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Nelson Ng";
+export const SITE_DESCRIPTION = "Nelson Ng's personal website";
